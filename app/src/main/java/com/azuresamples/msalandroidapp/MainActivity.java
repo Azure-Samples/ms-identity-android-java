9e0c621f55f13974d551283c9d7f9d81b600f10d
Dome Pongmongkol <rapong@microsoft.com>
2019-08-09 16:45:11 -0700

    App with single account fragment

---
+++ app/src/main/java/com/azuresamples/msalandroidapp/MainActivity.java
@@ -0,0 +1,117 @@
+package com.azuresamples.msalandroidapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        OnFragmentInteractionListener{
+
+    enum AppFragment {
+        SingleAccount
+    }
+
+    private AppFragment mCurrentFragment;
+
+    private ConstraintLayout mContentMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContentMain = findViewById(R.id.content_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Set default fragment
+        navigationView.setCheckedItem(R.id.nav_single_account);
+        setCurrentFragment(AppFragment.SingleAccount);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(final MenuItem item) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) { }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) { }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                // Handle navigation view item clicks here.
+                int id = item.getItemId();
+
+                if (id == R.id.nav_single_account) {
+                    setCurrentFragment(AppFragment.SingleAccount);
+                }
+
+                drawer.removeDrawerListener(this);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) { }
+        });
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setCurrentFragment(final AppFragment newFragment){
+        if (newFragment == mCurrentFragment) {
+            return;
+        }
+
+        mCurrentFragment = newFragment;
+        setHeaderString(mCurrentFragment);
+        displayFragment(mCurrentFragment);
+    }
+
+    private void setHeaderString(final AppFragment fragment){
+        switch (fragment) {
+            case SingleAccount:
+                getSupportActionBar().setTitle("Single Account Mode");
+        }
+    }
+
+    private void displayFragment(final AppFragment fragment){
+        switch (fragment) {
+            case SingleAccount:
+                attachFragment(new SingleAccountModeFragment());
+        }
+    }
+
+    private void attachFragment(final Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(mContentMain.getId(),fragment)
+                .commit();
+    }
+}
