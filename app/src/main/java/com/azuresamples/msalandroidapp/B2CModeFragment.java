e8e1d23dc3f84afa155b8448c4cbc0329432de3c
Dome Pongmongkol <rapong@microsoft.com>
2019-09-20 17:54:59 -0700

    Address comments

--- app/src/main/java/com/azuresamples/msalandroidapp/B2CModeFragment.java
+++ app/src/main/java/com/azuresamples/msalandroidapp/B2CModeFragment.java
@@ -1,3 +1,26 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package com.azuresamples.msalandroidapp;
 
 import android.os.Bundle;
@@ -249,6 +272,13 @@ public class B2CModeFragment extends Fragment {
 
             @Override
             public void onError(MsalException exception) {
+                final String B2C_PASSWORD_CHANGE = "AADB2C90118";
+                if (exception.getMessage().contains(B2C_PASSWORD_CHANGE)) {
+                    logTextView.setText("The user clicks the 'Forgot Password' link in a sign-up or sign-in user flow.\n" +
+                            "Your application needs to handle this error code by running a specific user flow that resets the password.");
+                    return;
+                }
+
                 /* Failed to acquireToken */
                 Log.d(TAG, "Authentication failed: " + exception.toString());
                 displayError(exception);
