d75886ce427fb55210297162d8ad6c5d7a18c48c
Jason Johnston <jasonjoh@users.noreply.github.com>
2021-04-06 12:30:34 -0400

    Update StaticTokenAuthProvider.java

--- app/src/main/java/com/azuresamples/msalandroidapp/StaticTokenAuthProvider.java
+++ app/src/main/java/com/azuresamples/msalandroidapp/StaticTokenAuthProvider.java
@@ -60,6 +60,6 @@ public class StaticTokenAuthProvider extends BaseAuthenticationProvider {
         if (shouldAuthenticateRequestWithUrl(requestUrl)) {
             return CompletableFuture.completedFuture(mAccessToken);
         }
-        return null;
+        return CompletableFuture.completedFuture(null);
     }
 }
